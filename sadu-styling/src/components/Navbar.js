7cf9e97860d613809ca1ce7b673692576c4708dc
tonioshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2023-12-08 00:13:45 -0500

    Fix navbar to disaapear when scrolling.

--- sadu-styling/src/components/Navbar.js
+++ sadu-styling/src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Navbar.css'; // Import the CSS file
 
 const Navbar = () => {
+    const navbarRef = useRef(null);
+
+    useEffect(() => {
+        const checkScroll = () => {
+            if (window.scrollY > window.innerHeight * 0.75) {
+                navbarRef.current.classList.add('hidden');
+            } else {
+                navbarRef.current.classList.remove('hidden');
+            }
+        };
+
+        window.addEventListener('scroll', checkScroll);
+
+        // Cleanup function to remove the event listener
+        return () => window.removeEventListener('scroll', checkScroll);
+    }, []);
+
     return (
-      <div className="navbar">
-        <a href="/">
-            <img src="/sadu-logo.png" alt="Logo" className="navbar-logo" />
-        </a>
-        <div className="navbar-links">
-          <a href="/">Home</a>
-          <a href="/services">Services</a>
-          <a href="/gallery">Gallery</a>
-          <a href="/contact">Contact</a>
+        <div className="navbar" ref={navbarRef}>
+            <a href="/">
+                <img src="/sadu-logo.png" alt="Logo" className="navbar-logo" />
+            </a>
+            <div className="navbar-links">
+                <a href="/">Home</a>
+                <a href="/services">Services</a>
+                <a href="/gallery">Gallery</a>
+                <a href="/contact">Contact</a>
+            </div>
         </div>
-      </div>
     );
-  };
-  
-  export default Navbar;
+};
+
+export default Navbar;
